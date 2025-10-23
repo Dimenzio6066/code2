5f47e9e8d198e90d911299def6f2ac0ec7b821c4
Dimenzio6066 <lukasniemann05@gmail.com>
2025-10-23 14:13:28 +0200

    Now I did it for real

---
+++ LinterTest/LinterTest.js
@@ -0,0 +1,18 @@
+"use strict";
+var linterTest;
+(function (linterTest) {
+    let key;
+    (function (key) {
+        key[key["Pos"] = 1] = "Pos";
+        key[key["neg"] = -1] = "neg";
+    })(key || (key = {}));
+    let info = { text: "G`udetmvhsgBncd1 ", key: key.Pos };
+    console.log(deCrypt(info.text, info.key));
+    function deCrypt(text, _Key) {
+        let result = "";
+        for (let i = 0; i < text.length; i++) {
+            result += String.fromCharCode(text.charCodeAt(i) + _Key);
+        }
+        return result;
+    }
+})(linterTest || (linterTest = {}));
