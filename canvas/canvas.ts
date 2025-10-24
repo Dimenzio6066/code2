5c9b7a11f6c12f8a55c118dce83d23453fc748c6
Dimenzio6066 <lukasniemann05@gmail.com>
2025-10-24 16:46:31 +0200

    New Task with canvas

---
+++ canvas/canvas.ts
@@ -0,0 +1,25 @@
+namespace Canvas {
+  window.addEventListener("load", hndDraw)
+
+  function hndDraw() {
+
+    let canvas: HTMLCanvasElement = document.querySelector("canvas")!;
+    let crc2: CanvasRenderingContext2D = canvas.getContext("2d")!;
+    crc2.fillStyle = "#5b39abff";
+    crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    crc2.beginPath();
+    crc2.arc(100, 100, 20, 0, 1.5 * Math.PI);
+    let path: Path2D = new Path2D();
+    path.arc(90, 70, 66, 0, 2 * Math.PI);
+    crc2.stroke(path);
+    crc2.closePath();
+    crc2.stroke();
+    crc2.beginPath();
+    crc2.ellipse(100, 40, 34, 9, 1, 0, 2 * Math.PI, true);
+    crc2.moveTo(210, 10);
+    crc2.lineTo(20, 299);
+    crc2.closePath();
+    crc2.stroke();
+
+  }
+}
