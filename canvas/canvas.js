11cffbbe539e0c00d64790efc0d345a50983a46e
Dimenzio6066 <lukasniemann05@gmail.com>
2025-10-27 17:15:31 +0100

    Added Coordinate System

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -10,27 +10,44 @@ var Canvas;
         crc2 = canvas.getContext("2d");
         canvas.width = 1640;
         canvas.height = 900;
-        crc2.fillStyle = "#5b39abff";
+        crc2.fillStyle = "#6c0effff";
         crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-        drawTriangle();
-        console.log(canvas.width);
-        console.log(canvas.height);
+        // let maxTriangleCount: number = 300;
+        // for (let i = 0; i < maxTriangleCount; i++) {
+        //   // drawTriangle();
+        // }
+        drawAxis();
     }
     // Draw
-    function drawTriangle() {
-        let pointA = { x: randomIntInRange(0, canvas.width), y: randomIntInRange(0, canvas.height) };
-        let pointB = { x: randomIntInRange(0, canvas.width), y: randomIntInRange(0, canvas.height) };
-        let pointC = { x: randomIntInRange(0, canvas.width), y: randomIntInRange(0, canvas.height) };
-        let path = new Path2D();
-        console.log(pointA);
-        path.moveTo(pointA.x, pointA.y);
-        path.lineTo(pointB.x, pointB.y);
-        path.lineTo(pointC.x, pointC.y);
-        path.lineTo(pointA.x, pointA.y);
-        path.closePath();
-        crc2.stroke(path);
-        crc2.fillStyle = "#bea341ff";
-        crc2.fill(path);
+    // function drawTriangle() {
+    //   let pointA: Vector2 = { x: randomIntInRange(0, canvas.width), y: randomIntInRange(0, canvas.height) };
+    //   let pointB: Vector2 = { x: randomIntInRange(0, canvas.width), y: randomIntInRange(0, canvas.height) };
+    //   let pointC: Vector2 = { x: randomIntInRange(0, canvas.width), y: randomIntInRange(0, canvas.height) };
+    //   let path: Path2D = new Path2D();
+    //   path.moveTo(pointA.x, pointA.y);
+    //   path.lineTo(pointB.x, pointB.y);
+    //   path.lineTo(pointC.x, pointC.y);
+    //   path.lineTo(pointA.x, pointA.y);
+    //   path.closePath();
+    //   crc2.stroke(path);
+    //   crc2.fillStyle = "#bea34176";11
+    //   crc2.fill(path);
+    // }
+    function drawAxis() {
+        let coordinateAxes = new Path2D();
+        let axisLength = 200;
+        coordinateAxes.moveTo(0, 0);
+        coordinateAxes.lineTo(0, axisLength);
+        coordinateAxes.moveTo(0, 0);
+        coordinateAxes.lineTo(axisLength, 0);
+        for (let i = 0; i < axisLength; i = i + 10) {
+            coordinateAxes.moveTo(0, i);
+            coordinateAxes.lineTo(4, i);
+            coordinateAxes.moveTo(i, 0);
+            coordinateAxes.lineTo(i, 4);
+        }
+        coordinateAxes.closePath();
+        crc2.stroke(coordinateAxes);
     }
     // Math
     function randomIntInRange(_min, _max) {
--- canvas/canvas.ts
+++ canvas/canvas.ts
@@ -11,34 +11,62 @@ namespace Canvas {
     canvas.width = 1640;
     canvas.height = 900;
 
-    crc2.fillStyle = "#5b39abff";
+    crc2.fillStyle = "#6c0effff";
     crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
 
-    drawTriangle();
+    // let maxTriangleCount: number = 300;
+    // for (let i = 0; i < maxTriangleCount; i++) {
+    //   // drawTriangle();
+    // }
 
-    console.log(canvas.width);
-    console.log(canvas.height);
+    drawAxis();
+
   }
 
   // Draw
-  function drawTriangle() {
-    let pointA: Vector2 = { x: randomIntInRange(0, canvas.width), y: randomIntInRange(0, canvas.height) };
-    let pointB: Vector2 = { x: randomIntInRange(0, canvas.width), y: randomIntInRange(0, canvas.height) };
-    let pointC: Vector2 = { x: randomIntInRange(0, canvas.width), y: randomIntInRange(0, canvas.height) };
-    let path: Path2D = new Path2D();
-    console.log(pointA);
+  // function drawTriangle() {
+  //   let pointA: Vector2 = { x: randomIntInRange(0, canvas.width), y: randomIntInRange(0, canvas.height) };
+  //   let pointB: Vector2 = { x: randomIntInRange(0, canvas.width), y: randomIntInRange(0, canvas.height) };
+  //   let pointC: Vector2 = { x: randomIntInRange(0, canvas.width), y: randomIntInRange(0, canvas.height) };
+  //   let path: Path2D = new Path2D();
 
 
-    path.moveTo(pointA.x, pointA.y);
-    path.lineTo(pointB.x, pointB.y);
-    path.lineTo(pointC.x, pointC.y);
-    path.lineTo(pointA.x, pointA.y);
-    path.closePath();
 
-    crc2.stroke(path);
-    crc2.fillStyle = "#bea341ff"
-    crc2.fill(path);
+  //   path.moveTo(pointA.x, pointA.y);
+  //   path.lineTo(pointB.x, pointB.y);
+  //   path.lineTo(pointC.x, pointC.y);
+  //   path.lineTo(pointA.x, pointA.y);
+  //   path.closePath();
+
+  //   crc2.stroke(path);
+  //   crc2.fillStyle = "#bea34176";11
+  //   crc2.fill(path);
+  // }
+
+  function drawAxis() {
+    let coordinateAxes: Path2D = new Path2D();
+    let axisLength: number = 200;
+    coordinateAxes.moveTo(0, 0);
+    coordinateAxes.lineTo(0, axisLength);
+    coordinateAxes.moveTo(0, 0);
+    coordinateAxes.lineTo(axisLength, 0);
+
+    for (let i = 0; i < axisLength; i = i+ 10) {
+      coordinateAxes.moveTo(0,i);
+      coordinateAxes.lineTo(4,i);
+      coordinateAxes.moveTo(i,0);
+      coordinateAxes.lineTo(i,4);
+    }
+
+
+    coordinateAxes.closePath();
+
+      crc2.stroke(coordinateAxes);
+
   }
+
+
+
 
   // Math
   function randomIntInRange(_min: number, _max: number): number {
