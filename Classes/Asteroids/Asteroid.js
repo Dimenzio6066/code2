9f7cd668a56a4df6efc082d93b6a894c43da038b
Dimenzio6066 <lukasniemann05@gmail.com>
2025-10-31 09:39:34 +0100

    Asteroid implementation

---
+++ Classes/Asteroids/Asteroid.js
@@ -0,0 +1,51 @@
+"use strict";
+var Assteroids;
+(function (Assteroids) {
+    class Asteroid {
+        position = new Assteroids.Vector(0, 0);
+        velocity = new Assteroids.Vector(0, 0);
+        type = 0;
+        size = 0;
+        constructor(_size, _position) {
+            // console.log("Asteroid constructor");
+            if (_position)
+                this.position = _position;
+            else
+                this.position = new Assteroids.Vector(0, 0);
+            this.velocity = new Assteroids.Vector(0, 0);
+            this.velocity.random(500, 1000);
+            this.type = Assteroids.randomIntInRange(0, 3);
+            this.size = _size;
+        }
+        move(_timselice) {
+            // console.log("Asteroid move");
+            const offset = new Assteroids.Vector(this.velocity.x, this.velocity.y);
+            offset.scale(_timselice);
+            this.position.add(offset);
+            if (this.position.x < 0)
+                this.position.x += Assteroids.crc2.canvas.width;
+            if (this.position.y < 0)
+                this.position.y += Assteroids.crc2.canvas.height;
+            if (this.position.x > Assteroids.crc2.canvas.width)
+                this.position.x -= Assteroids.crc2.canvas.width;
+            if (this.position.y > Assteroids.crc2.canvas.height)
+                this.position.y -= Assteroids.crc2.canvas.height;
+        }
+        draw() {
+            // console.log("Asteroid draw");
+            Assteroids.crc2.save();
+            Assteroids.crc2.translate(this.position.x, this.position.y);
+            Assteroids.crc2.scale(this.size, this.size);
+            Assteroids.crc2.translate(-50, -50);
+            Assteroids.crc2.stroke(Assteroids.asteroidPaths[this.type]);
+            Assteroids.crc2.restore();
+        }
+        isHit(_hotspot) {
+            const hitsize = 50 * this.size;
+            const difference = new Assteroids.Vector(_hotspot.x - this.position.x, _hotspot.y - this.position.y);
+            return (Math.abs(difference.x) < hitsize && Math.abs(difference.y) < hitsize);
+        }
+    }
+    Assteroids.Asteroid = Asteroid;
+})(Assteroids || (Assteroids = {}));
+//# sourceMappingURL=Asteroid.js.map
